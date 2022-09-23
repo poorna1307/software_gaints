da8f70f1c939748354bf995c626d9bcf64274a9a
Thumu Poorna Chander <poornachander.thumu13@gmail.com>
2022-09-23 16:12:21 +0530

    home nd wildfire added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {Nav, Navbar, Offcanvas, Container, Button, NavDropdown, Form} from 'react-bootstrap'
+import "./App.css"
+import {Route, Routes,NavLink} from "react-router-dom"
+import Home from './components/home/Home';
+import Wildfire from './components/wildfire/Wildfire'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar expand="lg" className="mb-3 head">
+          <Container fluid>
+            <Navbar.Brand href='' className='text-light'>NASA Earth</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-lg`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+                <Nav>
+                  <NavLink to=""  className='text-light nav-link'>Home</NavLink>
+                  <NavLink to="wildfire" className='text-light nav-link'>WildFire</NavLink>
+                  </Nav>  
+                  <NavDropdown 
+                    title="More "
+                    id={`offcanvasNavbarDropdown-expand-lg`}
+                  >
+                    <NavDropdown.Item >More</NavDropdown.Item>
+                    <NavDropdown.Item >
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/wildfire' element={<Wildfire />} />
+        </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/wildfire/Wildfire.js
@@ -0,0 +1,8 @@
+function Wildfire(){
+    return(
+        <>
+        <h1>Wild fire</h1>
+        </>
+    )
+}
+export default Wildfire;
